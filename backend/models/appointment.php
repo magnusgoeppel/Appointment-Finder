35427ce667c8d7532f1286d06fc051affbd1a201
Magnus <magnus_goeppel@protonmail.com>
2023-04-19 20:07:00 +0200

    erstellen der Datenbank

--- backend/models/appointment.php
+++ backend/models/appointment.php
@@ -1,1 +1,19 @@
 <?php
+class appointment {
+    public $id;
+    public $firstname;
+    public $lastname;
+    public $email;
+    public $phone;
+    public $department;
+
+    function __construct($id, $fn, $ln, $mail, $phone, $dept) {
+        $this->id = $id;
+        $this->firstname = $fn;
+        $this->lastname=$ln;
+        $this->email=$mail;
+        $this->phone=$phone;
+        $this->department=$dept;
+
+    }
+}
